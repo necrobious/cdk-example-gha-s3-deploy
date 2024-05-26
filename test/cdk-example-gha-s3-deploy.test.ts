3d885e71ecf14cce6345e1165d0a244e52cb0fae
Kirk Peterson <kirk@ciphercreaft.com>
2024-05-26 14:23:53 -0700

    Initial commit

---
+++ test/cdk-example-gha-s3-deploy.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkExampleGhaS3Deploy from '../lib/cdk-example-gha-s3-deploy-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-example-gha-s3-deploy-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkExampleGhaS3Deploy.CdkExampleGhaS3DeployStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
